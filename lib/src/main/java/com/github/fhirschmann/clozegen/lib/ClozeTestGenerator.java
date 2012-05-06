e95d71748dc571dcd23c888307e84fe5f592fa67
Fabian Hirschmann <fabian@hirschm.net>
2012-05-06 15:18:34 +0200

    ClozeTestGenerator: more checkstyle suggested changes.

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/ClozeTestGenerator.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/ClozeTestGenerator.java
@@ -3,18 +3,14 @@ package com.github.fhirschmann.clozegen.lib;
 import com.github.fhirschmann.clozegen.lib.annotators.ArticleDistractorAnnotator;
 import com.github.fhirschmann.clozegen.lib.annotators.PrepositionDistractorAnnotator;
 import com.github.fhirschmann.clozegen.lib.io.DebugWriter;
-import com.github.fhirschmann.clozegen.lib.io.InputReader;
 
 import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordSegmenter;
 import de.tudarmstadt.ukp.dkpro.core.treetagger.TreeTaggerPosLemmaTT4J;
-import java.io.File;
 import java.io.IOException;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_component.AnalysisComponent;
-import org.apache.uima.collection.CollectionReader;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
-import static org.uimafit.factory.CollectionReaderFactory.createCollectionReader;
 import org.uimafit.factory.JCasFactory;
 
 /**
@@ -22,16 +18,20 @@ import org.uimafit.factory.JCasFactory;
  * @author Fabian Hirschmann <fabian@hirschm.net>
  */
 public class ClozeTestGenerator {
-
+    /** The segmenter (sentence splitter) to use. */
     private Class<? extends AnalysisComponent> segmenter = StanfordSegmenter.class;
+
+    /** The tagger to use. */
     private Class<? extends AnalysisComponent> tagger = TreeTaggerPosLemmaTT4J.class;
+
+    /** The pipeline steps (segmenter, tagger, word class annotators). */
     private Pipeline pipeline = new Pipeline();
 
     static {
         org.apache.log4j.BasicConfigurator.configure();
     }
 
-    public void run() throws ResourceInitializationException, UIMAException,
+    public final void run() throws ResourceInitializationException, UIMAException,
             IOException, ClozegenException {
         JCas jcas = JCasFactory.createJCas();
         jcas.setDocumentText("I'd like a chicken!");
@@ -50,42 +50,42 @@ public class ClozeTestGenerator {
     /**
      * @return the segmenter
      */
-    public Class<? extends AnalysisComponent> getSegmenter() {
+    public final Class<? extends AnalysisComponent> getSegmenter() {
         return segmenter;
     }
 
     /**
      * @param segmenter the segmenter to set
      */
-    public void setSegmenter(Class<? extends AnalysisComponent> segmenter) {
+    public final void setSegmenter(final Class<? extends AnalysisComponent> segmenter) {
         this.segmenter = segmenter;
     }
 
     /**
      * @return the tagger
      */
-    public Class<? extends AnalysisComponent> getTagger() {
+    public final Class<? extends AnalysisComponent> getTagger() {
         return tagger;
     }
 
     /**
      * @param tagger the tagger to set
      */
-    public void setTagger(Class<? extends AnalysisComponent> tagger) {
+    public final void setTagger(final Class<? extends AnalysisComponent> tagger) {
         this.tagger = tagger;
     }
 
     /**
      * @return the pipeline
      */
-    public Pipeline getPipeline() {
+    public final Pipeline getPipeline() {
         return pipeline;
     }
 
     /**
      * @param pipeline the pipeline to set
      */
-    public void setPipeline(Pipeline pipeline) {
+    public final void setPipeline(final Pipeline pipeline) {
         this.pipeline = pipeline;
     }
 }
