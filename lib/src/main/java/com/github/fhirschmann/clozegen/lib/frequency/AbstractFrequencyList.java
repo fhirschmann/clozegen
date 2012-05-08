b91673d3401933d5401bd4b9da129794f1b5afd8
Fabian Hirschmann <fabian@hirschm.net>
2012-05-08 23:19:01 +0200

    documented AbstractFrequencyList

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/frequency/AbstractFrequencyList.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/frequency/AbstractFrequencyList.java
@@ -25,14 +25,42 @@ import java.util.List;
 import nu.xom.*;
 
 /**
+ * Base class for all Frequency Distribution Lists.
+ * <p>
+ * This base class allows you to load a list of frequencies from an XML
+ * file. These XML files must be of the following nature:
+ * <pre>
+ * <frequencies>
+ *     <frequency value="1234" ... />
+ *     <frequency value="4563" ... />
+ * </frequencies>
+ * </pre>
+ * The only required xml attribute here is the "value" attribute which indicates
+ * how many times the subject has occurred. You can add as many attributes
+ * as you like and process them in processChild(), so the population
+ * of the frequencies with ValueCount<T> is up to you.
  *
  * @author Fabian Hirschmann <fabian@hirschm.net>
  */
 public abstract class AbstractFrequencyList<T> {
+    /** The frequency count. */
     private final List<ValueCount<T>> frequencies = new ArrayList<ValueCount<T>>();
 
+    /**
+     * This method lets you process individual frequency-children in the XML file.
+     *
+     * @param child
+     */
     abstract void processChild(Element child);
 
+    /**
+     * Loads the frequency list from an XML file.
+     *
+     * @param xmlFile
+     * @throws ParsingException
+     * @throws ValidityException
+     * @throws IOException
+     */
     public void load(final String xmlFile)
             throws ParsingException, ValidityException, IOException {
         final InputStream stream = this.getClass().getClassLoader().
