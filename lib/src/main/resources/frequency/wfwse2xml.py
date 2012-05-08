6d6df71f9a4bc06e0b70b6c12579b4718efd3c1d
Fabian Hirschmann <fabian@hirschm.net>
2012-05-08 19:10:25 +0200

    added python script to generate frequency xml

---
+++ lib/src/main/resources/frequency/wfwse2xml.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# This script will generate a XML file from Word Frequencies
+# in Written and Spoken English [1] to be used with the
+# FrequencyList class in clozegen-lib.
+#
+# [1] http://ucrel.lancs.ac.uk/bncfreq/
+
+import requests
+from xml.etree.ElementTree import Element, SubElement, ElementTree
+
+
+FREQUENCIES = [
+        ('http://ucrel.lancs.ac.uk/bncfreq/lists/5_8_all_rank_preposition.txt', 'prepositions.xml')
+        ]
+
+
+def main(url, xml):
+    req = requests.get(url)
+
+    root = Element('frequencies')
+    tree = ElementTree(root)
+
+    for line in req.text.strip().split('\n'):
+        raw = line.strip().split('\t')
+
+        SubElement(root, 'frequency',
+                attrib={'sofa': raw[0], 'value': raw[1]})
+
+    tree.write(xml)
+
+if __name__ == '__main__':
+    for frequency in FREQUENCIES:
+        main(*frequency)
