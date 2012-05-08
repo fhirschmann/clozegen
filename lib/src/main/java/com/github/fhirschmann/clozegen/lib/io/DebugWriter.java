d47642530ea8dcad9a15ae152083f9e0e3879d6f
Fabian Hirschmann <fabian@hirschm.net>
2012-05-08 14:20:29 +0200

    DebugWriter: fixes as suggested by PMD

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/io/DebugWriter.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/io/DebugWriter.java
@@ -11,7 +11,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received annotation copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
@@ -28,7 +28,7 @@ import org.uimafit.component.JCasConsumer_ImplBase;
 import org.uimafit.util.FSCollectionFactory;
 
 /**
- * Logs a document by displaying its token's types, names, the text
+ * Logs annotation document by displaying its token's types, names, the text
  * covered, and in case of GapAnnotations, the valid and invalid answer options.
  *
  * @author Fabian Hirschmann <fabian@hirschm.net>
@@ -37,22 +37,27 @@ public class DebugWriter extends JCasConsumer_ImplBase {
 
     @Override
     public final void process(final JCas jCas) throws AnalysisEngineProcessException {
-        StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
 
         sb.append(String.format("%s%n", jCas.getDocumentText()));
 
         for (Iterator<Annotation> i = jCas.getAnnotationIndex().iterator(); i.hasNext();) {
-            Annotation a = i.next();
-            sb.append(String.format("[%s] (%s,%s) %s", a.getType().getShortName(),
-                    a.getBegin(), a.getEnd(), a.getCoveredText()));
-            if (a.getTypeIndexID() == GapAnnotation.type) {
-                GapAnnotation d = (GapAnnotation) a;
+            final Annotation annotation = i.next();
+            sb.append(String.format("[%s] (%s,%s) %s",
+                    annotation.getType().getShortName(),
+                    annotation.getBegin(),
+                    annotation.getEnd(),
+                    annotation.getCoveredText()));
+            if (annotation.getTypeIndexID() == GapAnnotation.type) {
+                final GapAnnotation gapAnnotation = (GapAnnotation) annotation;
                 sb.append(String.format(" %s%s%n",
-                        FSCollectionFactory.create(d.getValidAnswers()).toString(),
-                        FSCollectionFactory.create(d.getInvalidAnswers()).toString()));
+                        FSCollectionFactory.create(
+                        gapAnnotation.getValidAnswers()).toString(),
+                        FSCollectionFactory.create(
+                        gapAnnotation.getInvalidAnswers()).toString()));
             } else {
-                if (a.getTypeIndexID() == POS.type) {
-                    POS pos = (POS) a;
+                if (annotation.getTypeIndexID() == POS.type) {
+                    final POS pos = (POS) annotation;
                     sb.append(String.format(" [%s]", pos.getPosValue()));
                 }
                 sb.append(String.format("%n"));
