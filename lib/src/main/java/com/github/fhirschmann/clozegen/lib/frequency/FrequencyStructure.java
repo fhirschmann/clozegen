80fdc28d996b4424871d8b58fcf7f768670620c6
Fabian Hirschmann <fabian@hirschm.net>
2012-05-11 14:56:18 +0200

    improved FrequencyStructure javadocs

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/frequency/FrequencyStructure.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/frequency/FrequencyStructure.java
@@ -42,7 +42,17 @@ import java.util.Map;
  * For fast lookup of an individual frequency, a HashMap is used. The Value
  * of this HashMap points to the FrequencyPair as it appears in the list.
  * </p>
+ * <p>
+ * This structure is NOT meant to count individual words in order to
+ * build a distribution frequency. This class can be used if the
+ * frequency of something is already known beforehand. If you require
+ * dynamic counting of words, you should take a look at
+ * <a href="http://code.google.com/p/guava-libraries/wiki/NewCollectionTypesExplained">
+ * Google Guava's Multisets
+ * </a>.
+ * </p>
  *
+ * @see <a href="http://www.glazedlists.com/">Glazed Lists</a>
  * @author Fabian Hirschmann <fabian@hirschm.net>
  */
 public class FrequencyStructure<V> implements Iterable {
