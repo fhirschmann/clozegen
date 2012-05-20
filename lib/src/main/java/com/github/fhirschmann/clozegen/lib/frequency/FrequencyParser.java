e9f69c8cd6ad5057e49cbe77b08df4739a96feb1
Fabian Hirschmann <fabian@hirschm.net>
2012-05-20 20:30:42 +0200

    moved frequency stuff to frequency package

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/frequency/FrequencyParser.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/frequency/FrequencyParser.java
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package com.github.fhirschmann.clozegen.lib.parser;
+package com.github.fhirschmann.clozegen.lib.frequency;
 
 import com.github.fhirschmann.clozegen.lib.multiset.MapMultiset;
 import com.google.common.base.Charsets;
