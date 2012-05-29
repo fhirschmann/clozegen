92fdb03be896fb4173d0211b9ef20d70ababa51c
Fabian Hirschmann <fabian@hirschm.net>
2012-05-29 22:27:53 +0200

    fixed typo

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/pipeline/Pipeline.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/pipeline/Pipeline.java
@@ -35,7 +35,7 @@ import static org.uimafit.factory.AnalysisEngineFactory.createPrimitiveDescripti
 
 /**
  * A pipeline is a chain of AnalysisEngines arranged so that each step will be run
- * sequentially with all steps steps working on the same CAS.
+ * sequentially with all steps working on the same CAS.
  * <p>
  * This class provides convenience methods to add steps from engines, engine
  * descriptions and engine components dynamically.
