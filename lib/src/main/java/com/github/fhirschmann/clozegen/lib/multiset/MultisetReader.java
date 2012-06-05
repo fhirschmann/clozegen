458819b865b3bf63ba777b0ef50618b6e009e415
Fabian Hirschmann <fabian@hirschm.net>
2012-06-05 15:33:55 +0200

    [lib] MultisetReader: improved documentation

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/multiset/MultisetReader.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/multiset/MultisetReader.java
@@ -32,7 +32,9 @@ import java.util.List;
  * @author Fabian Hirschmann <fabian@hirschm.net>
  */
 public final class MultisetReader {
-    private MultisetReader() {}
+    /** Constructor in utility class should not be called. */
+    private MultisetReader() {
+    }
 
     /**
      * Parses frequencies from a URL. The subject and the count for a subject
