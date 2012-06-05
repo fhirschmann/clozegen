4075ad7d9ffccd79daa803c69691d97d2ebbe27d
Fabian Hirschmann <fabian@hirschm.net>
2012-06-05 21:38:18 +0200

    removed unused imports

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/adapter/StupidArticleAdapter.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/adapter/StupidArticleAdapter.java
@@ -17,7 +17,6 @@
  */
 package com.github.fhirschmann.clozegen.lib.adapter;
 
-import com.github.fhirschmann.clozegen.lib.adapter.Adapter;
 import com.github.fhirschmann.clozegen.lib.generator.GapGenerator;
 import com.github.fhirschmann.clozegen.lib.generator.en.StupidArticleGapGenerator;
 import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.ART;
