df4b3e140e21ea5be96b1258b5445fb3c9c2445f
Fabian Hirschmann <fabian@hirschm.net>
2012-06-27 00:22:35 +0200

    [lib] implemented debug mode; comments

--- cli/src/main/java/com/github/fhirschmann/clozegen/cli/Main.java
+++ cli/src/main/java/com/github/fhirschmann/clozegen/cli/Main.java
@@ -19,12 +19,9 @@ package com.github.fhirschmann.clozegen.cli;
 
 import com.github.fhirschmann.clozegen.lib.register.DescriptionRegisterEntry;
 import com.github.fhirschmann.clozegen.lib.register.RegisterFactory;
-import java.io.IOException;
 import org.apache.commons.cli.*;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
-import org.apache.uima.UIMAException;
-import org.apache.uima.resource.ResourceInitializationException;
 import static com.google.common.base.Preconditions.checkArgument;
 import com.google.common.collect.Maps;
 import java.util.Map;
@@ -42,27 +39,44 @@ public class Main {
         org.apache.log4j.BasicConfigurator.configure();
     }
 
+    /**
+     * Creates a new instance.
+     */
     public Main() {
-        Logger logger = Logger.getRootLogger();
-        logger.setLevel(Level.INFO);
         formatter = new HelpFormatter();
         options = new Options();
     }
 
+    /**
+     * Prints the help message.
+     */
     public void printHelp() {
         formatter.printHelp("clozegen [options] INPUT OUTPUT", options);
-
     }
 
-    public void run(final String[] args)
-            throws ResourceInitializationException, UIMAException, IOException {
+    /**
+     * Runs the command line interface.
+     *
+     * @param args the arguments to the CLI
+     */
+    public void run(final String[] args) {
         CommandLineParser parser = new PosixParser();
         options.addOption("g", "generators", true, "generators to activate");
         options.addOption("h", "help", false, "print help message and exit");
         options.addOption("l", "list", false, "print all available gap generators");
+        options.addOption("d", "debug", false, "enable debug mode");
+        boolean debug = true;
+
 
         try {
             CommandLine line = parser.parse(options, args);
+
+            if (line.hasOption("debug")) {
+                Logger.getRootLogger().setLevel(Level.DEBUG);
+            } else {
+                Logger.getRootLogger().setLevel(Level.INFO);
+                debug = false;
+            }
 
             if (line.hasOption("help")) {
                 printHelp();
@@ -87,13 +101,22 @@ public class Main {
                 job.run(generators, line.getArgs()[0], line.getArgs()[1]);
             }
 
-        } catch (ParseException exp) {
-            System.err.println(exp.getMessage());
+        } catch (Exception exp) {
+            if (debug) {
+                exp.printStackTrace();
+            } else {
+                System.err.println("ERROR: " + exp.getMessage());
+            }
             printHelp();
         }
     }
 
-    public static void main(final String[] argv) throws Exception {
+    /**
+     * Passes the arguments to {@link Main#main(java.lang.String[])}.
+     *
+     * @param argv the arguments
+     */
+    public static void main(final String[] argv) {
         Main main = new Main();
         main.run(argv);
     }
