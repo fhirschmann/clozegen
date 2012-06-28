cf7af57ebc44889e907b2979d3794c1087290f4c
Fabian Hirschmann <fabian@hirschm.net>
2012-06-28 23:03:54 +0200

    [lib] fixed frequency gap generator + tests

--- lib/src/main/java/com/github/fhirschmann/clozegen/lib/generators/FrequencyGapGenerator.java
+++ lib/src/main/java/com/github/fhirschmann/clozegen/lib/generators/FrequencyGapGenerator.java
@@ -24,6 +24,10 @@ import com.github.fhirschmann.clozegen.lib.generators.model.MultisetModel;
 import com.github.fhirschmann.clozegen.lib.util.CollectionUtils;
 import com.github.fhirschmann.clozegen.lib.util.MultisetUtils;
 import com.google.common.base.Optional;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Multiset.Entry;
 import java.util.List;
 
 /**
@@ -55,13 +59,21 @@ public class FrequencyGapGenerator
 
     @Override
     public Optional<Gap> generate(final int count) {
-        List<String> candidates = MultisetUtils.
-                sortedElementList(model.getMultiset(), count);
-        if (candidates.contains(token)) {
-            int index = candidates.indexOf(token);
-            return Optional.of(Gap.with(token,
-                    CollectionUtils.getAdjacentTo(candidates, index, count).
-                    subList(0, count).toArray(new String[0])));
+        if (model.getMultiset().contains(token)) {
+            int tokenCount = model.getMultiset().count(token);
+            Multiset<String> ms = HashMultiset.create();
+
+            for (Entry<String> entry : model.getMultiset().entrySet()) {
+                ms.add(entry.getElement(), Math.abs(entry.getCount() - tokenCount));
+            }
+
+            if (ms.elementSet().size() < count - 1) {
+                return Optional.absent();
+            } else {
+                return Optional.of(
+                        Gap.with(token, Lists.reverse(
+                        MultisetUtils.sortedElementList(ms)).subList(0, count - 1)));
+            }
         } else {
             return Optional.absent();
         }
---
+++ lib/src/test/java/com/github/fhirschmann/clozegen/lib/generators/FrequencyGapGeneratorTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2012 Fabian Hirschmann <fabian@hirschm.net>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+package com.github.fhirschmann.clozegen.lib.generators;
+
+import com.github.fhirschmann.clozegen.lib.generators.api.Gap;
+import com.github.fhirschmann.clozegen.lib.generators.model.MultisetModel;
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ *
+ * @author Fabian Hirschmann <fabian@hirschm.net>
+ */
+public class FrequencyGapGeneratorTest {
+    private MultisetModel model;
+    private FrequencyGapGenerator generator;
+
+    @Before
+    public void setUp() {
+        model = new MultisetModel();
+        Multiset ms = HashMultiset.create();
+        ms.add("of", 100); // 40
+
+        ms.add("in", 70); // 10
+        ms.add("to", 60); // 0
+        ms.add("for", 50); // 10
+
+        ms.add("with", 30); // 30
+        ms.add("on", 10); // 50
+        model.setMultiset(ms);
+
+        generator = new FrequencyGapGenerator();
+        generator.initialize(model);
+    }
+
+    @Test
+    public void testGenerate() {
+        generator.initialize("to");
+        assertThat(generator.generate(3).get(), is(Gap.with("to", "in", "for")));
+    }
+
+    @Test
+    public void testGenerate2() {
+        generator.initialize("on");
+        assertThat(generator.generate(3).get(), is(Gap.with("on", "with", "for")));
+    }
+}
